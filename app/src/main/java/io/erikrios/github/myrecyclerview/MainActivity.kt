bb13bc5c2debdf1da9cce1f4888529ffa6b71820
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2020-12-28 10:51:18 +0700

    Integrated the view binding

--- app/src/main/java/io/erikrios/github/myrecyclerview/MainActivity.kt
+++ app/src/main/java/io/erikrios/github/myrecyclerview/MainActivity.kt
@@ -1,11 +1,18 @@
 package io.erikrios.github.myrecyclerview
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import io.erikrios.github.myrecyclerview.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.rvHeroes.setHasFixedSize(true)
     }
 }
