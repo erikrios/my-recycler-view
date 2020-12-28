2fde7d8fecc6ef8fb7e8b3d20874278f53551f40
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2020-12-28 10:38:29 +0700

    Create Hero.kt

---
+++ app/src/main/java/io/erikrios/github/myrecyclerview/models/Hero.kt
@@ -0,0 +1,11 @@
+package io.erikrios.github.myrecyclerview.models
+
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
+data class Hero(
+    var name: String,
+    var description: String,
+    var photo: String
+) : Parcelable
